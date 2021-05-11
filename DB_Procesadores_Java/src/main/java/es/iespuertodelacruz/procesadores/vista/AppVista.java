2b83fc51524c0299ede3a7356ddde7a6817b26d7
gersancabo <gersancabo@gmail.com>
2021-05-11 18:06:43 +0100

    #10 creamos el esqueleto del menu

--- DB_Procesadores_Java/src/main/java/es/iespuertodelacruz/procesadores/vista/AppVista.java
+++ DB_Procesadores_Java/src/main/java/es/iespuertodelacruz/procesadores/vista/AppVista.java
@@ -1,9 +1,55 @@
 package es.iespuertodelacruz.procesadores.vista;
 
-/**
- * Hello world!
- *
- */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class AppVista {
 
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        while (!salir) {
+ 
+            System.out.println("1. Insertar");
+            System.out.println("2. Modificar");
+            System.out.println("3. Eliminar");
+            System.out.println("4. Buscar");
+            System.out.println("5. ");
+            System.out.println("6. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has seleccionado la opcion 1");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion 3");
+                        break;
+                    case 4:
+                        System.out.println("Has seleccionado la opcion 4");
+                        break;
+                    case 5:
+                        System.out.println("Has seleccionado la opcion 5");
+                        break;
+                    case 6:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 6");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+    }
 }
