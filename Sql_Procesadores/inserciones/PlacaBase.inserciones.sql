f77f51931abacd90245fd15f8e6fde5c7dca8768
alejandropo1901 <alejandropo1901@gmail.com>
2021-05-21 17:15:18 +0100

    Creacion de Insert into del fichero placaBase.sql

--- Sql_Procesadores/inserciones/PlacaBase.inserciones.sql
+++ Sql_Procesadores/inserciones/PlacaBase.inserciones.sql
@@ -12,8 +12,23 @@ INSERT INTO placa_base (id,id_socket,nombre) VALUES (5,4,"Supermicro MBD-X12SPA-
 --placa base con tipo de socket lga 3647
 
 INSERT INTO placa_base (id,id_socket,nombre) VALUES (6,6,"Supermicro X11DPi-N");
-INSERT INTO placa_base (id,id_socket,nombre) VALUES (6,6,"ASUS Z11PR-D16");
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (7,6,"ASUS Z11PR-D16");
 
+--placa base con tipo de socket AM4
 
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (8,7,"Asus PRIME B450M-A II");
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (9,7,"Asus ROG Strix X570-E Gaming");
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (10,7,"Asus ROG Crosshair VIII Formula");
 
+--placa base con tipo de socket TRX4
+
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (11,8,"Msi TRX40 PRO");
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (12,8,"Asus TRX4 PRIME TRX40-PRO");
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (13,8,"ROG Zenith II Extreme");
+
+--placa base con tipo de socket TR4
+
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (14,9,"Gigabyte X399 Aorus PRO");
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (15,9,"Asrock X399 Taichi");
+INSERT INTO placa_base (id,id_socket,nombre) VALUES (16,9,"Asrock Fatal1ty X399");
 
