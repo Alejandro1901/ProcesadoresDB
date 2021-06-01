fcc268d5c7f48988d2ff16f3721f7c86f7b0a959
gersancabo <gersancabo@gmail.com>
2021-06-01 12:25:33 +0100

    #17 test fichero funcionando

--- DB_Procesadores_Java/src/test/java/es/iespuertodelacruz/procesadores/fichero/FicheroTest.java
+++ DB_Procesadores_Java/src/test/java/es/iespuertodelacruz/procesadores/fichero/FicheroTest.java
@@ -8,9 +8,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import es.iespuertodelacruz.procesadores.excepcion.FicheroException;
-import es.iespuertodelacruz.procesadores.fichero.*;
-
 public class FicheroTest {
 
     static Fichero fichero;
@@ -23,13 +20,8 @@ public class FicheroTest {
     @Test
     public void leerFicheroTest() {
         try {
-            fichero.leer("insertar.sql");
-        } catch (Exception e) {
-            assertTrue(e.getMessage().contains("El fichero a leer no existe"));
-        }
-        try {
-            String lectura = fichero.leer("resources/sql/sqlite/producto.crear.sql");
-            assertTrue(lectura.contains("CREATE TABLE producto ("));
+            String lectura = fichero.leer("resources/creaciones/arquitectura.crear.sql");
+            assertTrue(lectura.contains("CREATE TABLE IF NOT EXISTS arquitectura ("));
         } catch (Exception e) {
             fail("Se ha producido un error leyendo el fichero", e);
         }
